fdb230deb7a52b8b964fff4c92ced63738e85b3f
Mario Murrent <mobilemdm@live.at>
2013-10-14 14:32:57 +0200

    Modified logger

--- first_semester/programing/final_project/murrent/BiOWheels/BiOWheels/IContainer.cs
+++ first_semester/programing/final_project/murrent/BiOWheels/BiOWheels/IContainer.cs
@@ -2,11 +2,38 @@
 
 namespace BiOWheels
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IContainer
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TContract"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
         void Register<TContract, TImplementation>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TContract"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
+        /// <param name="instance"></param>
         void Register<TContract, TImplementation>(TImplementation instance);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         T Resolve<T>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contract"></param>
+        /// <returns></returns>
         object Resolve(Type contract);
     }
 }
--- first_semester/programing/final_project/murrent/BiOWheels/BiOWheels/SimpleContainer.cs
+++ first_semester/programing/final_project/murrent/BiOWheels/BiOWheels/SimpleContainer.cs
@@ -5,6 +5,9 @@ using System.Reflection;
 
 namespace BiOWheels
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class SimpleContainer : IContainer
     {
         public static readonly IContainer Instance = new SimpleContainer();
@@ -12,37 +15,42 @@ namespace BiOWheels
         private static readonly IDictionary<Type, Type> Types = new Dictionary<Type, Type>();
         private static readonly IDictionary<Type, object> TypeInstances = new Dictionary<Type, object>();
 
+        /// <inheritdoc/>
         public void Register<TContract, TImplementation>()
         {
             Types[typeof(TContract)] = typeof(TImplementation);
         }
+
+        /// <inheritdoc/>
         public void Register<TContract, TImplementation>(TImplementation instance)
         {
             TypeInstances[typeof(TContract)] = instance;
         }
+
+        /// <inheritdoc/>
         public T Resolve<T>()
         {
             return (T)Resolve(typeof(T));
         }
+
+        /// <inheritdoc/>
         public object Resolve(Type contract)
         {
             if (TypeInstances.ContainsKey(contract))
             {
                 return TypeInstances[contract];
             }
-            else
+
+            Type implementation = Types[contract];
+            ConstructorInfo constructor = implementation.GetConstructors()[0];
+            ParameterInfo[] constructorParameters = constructor.GetParameters();
+            if (constructorParameters.Length == 0)
             {
-                Type implementation = Types[contract];
-                ConstructorInfo constructor = implementation.GetConstructors()[0];
-                ParameterInfo[] constructorParameters = constructor.GetParameters();
-                if (constructorParameters.Length == 0)
-                {
-                    return Activator.CreateInstance(implementation);
-                }
-                List<object> parameters = new List<object>(constructorParameters.Length);
-                parameters.AddRange(constructorParameters.Select(parameterInfo => Resolve(parameterInfo.ParameterType)));
-                return constructor.Invoke(parameters.ToArray());
+                return Activator.CreateInstance(implementation);
             }
+            List<object> parameters = new List<object>(constructorParameters.Length);
+            parameters.AddRange(constructorParameters.Select(parameterInfo => Resolve(parameterInfo.ParameterType)));
+            return constructor.Invoke(parameters.ToArray());
         }
     }
 }
