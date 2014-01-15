14964c2c7e762cdce084fd18ff85d8c819e24da2
grill2010 <f.grill@live.at>
2014-01-15 17:12:02 +0100

    Order and search algorithm

---
+++ first_semester/algo_dat/flo/SortApplication/SortApplication/SearchAlgo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortApplication
+{
+    public class SearchAlgo
+    {
+        public int linearSearch(int[] array, int search)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == search)
+                {
+                    return search;
+                }
+            }
+            return -1;
+        }
+
+        public int BinarySearch(int[] array, int lowBound, int highBound, int search)
+        {
+            int mid;
+            while (lowBound <= highBound)
+            {
+                mid = (lowBound + highBound) / 2;
+                if (array[mid] < search)//the element we search is located to the right from the mid point
+                {
+                    lowBound = mid + 1;
+                    continue;
+                }
+                else if (array[mid] > search)//the element we search is located to the left from the mid point
+                {
+                    highBound = mid - 1;
+                    continue;
+                }
+                //at this point low and high bound are equal and we have found the element or
+                //array[mid] is just equal to the search => we have found the searched element
+                else
+                {
+                    return array[mid];
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ first_semester/algo_dat/flo/SortApplication/TestSortApplication/UnitTest.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SortApplication;
+
+namespace TestSortApplication
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        private SortAlgo sortAlgoClass;
+        private SortHeap sortHeapClass;
+        private SearchAlgo searchAlgoClass;
+        List<long> times = new List<long>();
+
+        [SetUp]
+        public void Init()
+        {
+            sortAlgoClass = new SortAlgo();
+            sortHeapClass = new SortHeap();
+            searchAlgoClass = new SearchAlgo();
+        }
+
+        [TestCase]
+        public void TestSelectionSort()
+        {
+            int maxValue = 100;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int[] array = GenerateArray(InitialiseArray(maxValue), 0, maxValue);
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    int[] result = sortAlgoClass.selectionSort(array);
+                    sw.Stop();
+                    OutputTime(String.Concat("Array Length: ", maxValue), sw);
+                }
+                maxValue *= 10;
+                OutputTimes();
+            }
+        }
+
+        [TestCase]
+        public void TestBubbleSort()
+        {
+            int maxValue = 100;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int[] array = GenerateArray(InitialiseArray(maxValue), 0, maxValue);
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    int[] result = sortAlgoClass.bubbleSort(array);
+                    sw.Stop();
+                    OutputTime(String.Concat("Array Length: ", maxValue), sw);
+                }
+                maxValue *= 10;
+                OutputTimes();
+            }
+        }
+
+        [TestCase]
+        public void TestInsertionSort()
+        {
+            int maxValue = 100;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int[] array = GenerateArray(InitialiseArray(maxValue), 0, maxValue);
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    int[] result = sortAlgoClass.insertionSort(array);
+                    sw.Stop();
+                    OutputTime(String.Concat("Array Length: ", maxValue), sw);
+                }
+                maxValue *= 10;
+                OutputTimes();
+            }
+        }
+
+        [TestCase]
+        public void TestHeapSort()
+        {
+            int maxValue = 100;
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int[] array = GenerateArray(InitialiseArray(maxValue), 0, maxValue);
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    int[] result = sortHeapClass.heapSort(array);
+                    sw.Stop();
+                    OutputTime(String.Concat("Array Length: ", maxValue), sw);
+                }
+                maxValue *= 10;
+                OutputTimes();
+            }
+        }
+
+        [TestCase]
+        public void TestLinearSearch()
+        {
+            int maxValue = 1000000;
+            Random randNum = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int[] array = GenerateArray(InitialiseArray(maxValue), 0, maxValue);
+                int randNumber = randNum.Next(0, maxValue);
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                int foundNumber = searchAlgoClass.linearSearch(array, randNumber);
+                sw.Stop();
+                if (foundNumber == randNumber)
+                {
+                    Console.WriteLine(String.Concat("Number ", foundNumber, " found in ", sw.ElapsedMilliseconds, " msc"));
+                }
+                else
+                {
+                    Console.WriteLine(String.Concat("Number not found! ", sw.ElapsedMilliseconds, " msc"));
+                }
+            }
+        }
+
+        [TestCase]
+        public void TestBinarySearch()
+        {
+            int maxValue = 100000;
+            Random randNum = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int[] array = sortAlgoClass.insertionSort(GenerateArray(InitialiseArray(maxValue), 0, maxValue)); // First sort array because binary search works only with sorted arrays
+                int randNumber = randNum.Next(0, maxValue);
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                int foundNumber = searchAlgoClass.BinarySearch(array, 0, array.Length, randNumber);
+                sw.Stop();
+                if (foundNumber == randNumber)
+                {
+                    Console.WriteLine(String.Concat("Number ", foundNumber, " found in ", sw.ElapsedMilliseconds, " msc"));
+                }
+                else
+                {
+                    Console.WriteLine(String.Concat("Number ", randNumber, " not found, need", sw.ElapsedMilliseconds, " msc"));
+                }
+            }
+        }
+
+        private int[] InitialiseArray(int maxCount)
+        {
+            return new int[maxCount];
+        }
+
+        private int[] GenerateArray(int[] array, int min, int max)
+        {
+            Random randNum = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randNum.Next(min, max);
+            }
+            return array;
+        }
+
+        private void OutputTime(string message, Stopwatch sw)
+        {
+            times.Add(sw.ElapsedMilliseconds);
+            Console.WriteLine(message +
+                ": Time StopWatch: " + sw.Elapsed.ToString()
+                + " msecs " + sw.ElapsedMilliseconds.ToString()
+                + " ticks " + sw.ElapsedTicks.ToString());
+        }
+
+        private void OutputTimes()
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------***********------------");
+            Console.WriteLine("Min Time was: " + times.Min() + " msc");
+            Console.WriteLine("Average Time was: " + times.Average() + " msc");
+            Console.WriteLine("Max Time was: " + times.Max() + " msc");
+            Console.WriteLine("------------***********------------");
+            Console.WriteLine();
+        }
+    }
+}
