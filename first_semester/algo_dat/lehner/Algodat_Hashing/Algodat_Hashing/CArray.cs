85a1fcc0d6a3573aa80b1e9ada790ca04a32dc17
RoLeh <rlehner1988@gmail.com>
2014-02-17 21:02:49 +0100

    - added pseudo CArray class.

---
+++ first_semester/algo_dat/lehner/Algodat_Hashing/Algodat_Hashing/CArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algodat_Hashing
+{
+    public class CArray
+    {
+        private List<string> textArray { get; set; }
+
+        public CArray()
+        {
+            this.textArray = new List<string>();
+        }
+
+        public bool SearchForString(string str)
+        {
+            return this.textArray.Contains(str);
+        }
+
+        public void AddString(string str) 
+        {
+            this.textArray.Add(str);
+        }
+    }
+}
