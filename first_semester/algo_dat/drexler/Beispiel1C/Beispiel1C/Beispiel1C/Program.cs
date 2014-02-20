7a73d0f22710a2211daf1e88add6e6358f2d148c
Michael Drexler <drexler_michael@judo-eisenstadt.at>
2014-02-20 23:53:35 +0100

    Beispiel 1C: Berechne Faktorielle mit und ohne Rekursion

--- first_semester/algo_dat/drexler/Beispiel1C/Beispiel1C/Beispiel1C/Program.cs
+++ first_semester/algo_dat/drexler/Beispiel1C/Beispiel1C/Beispiel1C/Program.cs
@@ -18,33 +18,6 @@ namespace Beispiel1C
             long c = Fakultaet.BerechneOhneRekursion(1000000);
             Console.WriteLine(c.ToString());
             Console.ReadKey();
-            //Stopwatch sw = new Stopwatch();
-            //sw.Start();
-            //DisplayNums(nums);
-            //sw.Stop();
-
-            //Timing tm = new Timing();
-            //tm.StartTime();
-            //DisplayNums(nums);
-            //tm.StopTime();
-
-            /*
-            string input = string.Empty;
-            while (input != "exit")
-            {
-                input = Console.ReadLine();
-                int n = 0;
-                Int32.TryParse(input, out n);
-                int erwartetesErgebnis = 1;
-                for (int i = 1; i <= n; i++)
-                {
-                    erwartetesErgebnis *= i;
-                }
-                Console.WriteLine("Erwartetes Ergebnis: {0}", erwartetesErgebnis.ToString());
-                int result = Fakultaet.BerechneStandardRekusion(n);
-                Console.WriteLine("Ergebnis: {0}", result.ToString());
-            }
-            */
         }
     }
 }
--- first_semester/algo_dat/drexler/Beispiel1C/Beispiel1C/UnitTestBeispiel1C/UnitTestFakultaet.cs
+++ first_semester/algo_dat/drexler/Beispiel1C/Beispiel1C/UnitTestBeispiel1C/UnitTestFakultaet.cs
@@ -2,12 +2,47 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Beispiel1C;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace UnitTestBeispiel1C
 {
     [TestClass]
     public class UnitTestFakultaet
     {
+        private Dictionary<int, int> factorial;
+        private Stopwatch stopwatch = new Stopwatch();
+
+        [TestInitialize]
+        public void Init()
+        {
+            factorial = new Dictionary<int, int>
+            {
+                {0, 1},
+                {1, 1},
+                {2, 2},
+                {3, 6},
+                {4, 140},
+                {5, 550},
+                {6, 800},
+                {7, 1000},
+                {8, 2000},
+                {9, 4000},
+                {10, 8000},
+                {11, 16000},
+                {12, 32000},
+                {13, 64000},
+                {14, 128000},
+                {15, 256000},
+                {16, 512000},
+                {17, 1000000},
+                {18, 3000000},
+                {19, 1000000000},
+            };
+
+            this.stopwatch = new Stopwatch();
+        }
+
         [TestMethod]
         public void BerechneStandardReksuion()
         {
@@ -18,7 +53,92 @@ namespace UnitTestBeispiel1C
         [TestMethod]
         public void BerechneLimitStandardRekusion()
         {
+            long lastItem = 0;
             // Untersuche die Grenzen der Funktionen
+
+            try
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                foreach (int item in this.factorial.Keys)
+                {
+                    int value = 0;
+                    this.factorial.TryGetValue(item, out value);
+                    lastItem = item;
+                    long result = Fakultaet.BerechneStandardRekusion(value);
+                }
+            }
+            catch (StackOverflowException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                stopwatch.Stop();
+                Debug.WriteLine("Time for standard recursion {0}ms - Last item: {1}", stopwatch.ElapsedMilliseconds, lastItem);  
+            }      
+        }
+
+        [TestMethod]
+        public void BerechneLimitTailRekusion()
+        {
+            long lastItem = 0;
+            // Untersuche die Grenzen der Funktionen
+
+            try
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                foreach (int item in this.factorial.Keys)
+                {
+                    int value = 0;
+                    this.factorial.TryGetValue(item, out value);
+                    lastItem = item;
+                    long result = 0;
+                    long returnValue = Fakultaet.BerechneTailRekursion(value, result);
+                }
+            }
+            catch (StackOverflowException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                stopwatch.Stop();
+                Debug.WriteLine("Time for tail recursion {0}ms - Last item: {1}", stopwatch.ElapsedMilliseconds, lastItem);
+            }
+        }
+
+        [TestMethod]
+        public void BerechneLimitOhneRekusion()
+        {
+            long lastItem = 0;
+            // Untersuche die Grenzen der Funktionen
+
+            try
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                foreach (int item in this.factorial.Keys)
+                {
+                    int value = 0;
+                    this.factorial.TryGetValue(item, out value);
+                    lastItem = item;
+                    long returnValue = Fakultaet.BerechneOhneRekursion(value);
+                }
+            }
+            catch (StackOverflowException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                stopwatch.Stop();
+                Debug.WriteLine("Time for tail recursion {0}ms - Last item: {1}", stopwatch.ElapsedMilliseconds, lastItem);
+            }
         }
 
         [TestMethod]
