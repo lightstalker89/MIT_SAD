b1b64b4cd7edcd0c46c68a7b080652c3155933c8
Michael Drexler <drexler_michael@judo-eisenstadt.at>
2014-10-17 23:44:06 +0200

    Softwareengineering MVVM Sample

---
+++ 3Semester/MVVM_Sample/MVVM_Sample/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Sample.ViewModels
+{
+    public class ViewModelLocator
+    {
+
+        public MainWindow ViewModelMainWindow
+        {
+            get { return new MainWindow(); }
+        }
+    }
+}
