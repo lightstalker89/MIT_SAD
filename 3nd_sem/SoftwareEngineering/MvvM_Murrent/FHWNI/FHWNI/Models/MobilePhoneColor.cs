5e994612b8906281b4f4e01a1b1c15feea5c3003
unknown <MMurrent@FRQNB120697.frequentis.frq>
2014-10-19 11:51:41 +0200

    +) Added mvvm project

---
+++ 3nd_sem/SoftwareEngineering/MvvM_Murrent/FHWNI/FHWNI/Models/MobilePhoneColor.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media;
+
+namespace FHWNI.Models
+{
+    public class MobilePhoneColor
+    {
+        public string ColorName { get; set; }
+        public SolidColorBrush FillColor { get; set; }
+    }
+}
