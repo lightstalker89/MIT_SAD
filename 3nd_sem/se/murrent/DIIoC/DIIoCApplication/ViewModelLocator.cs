90823e152fba1fe937b0d959a3d179bafe7ee728
Mario Murrent <mobilemdm@live.at>
2014-11-06 21:31:43 +0100

    *) Update VS Solution for SE

---
+++ 3nd_sem/se/murrent/DIIoC/DIIoCApplication/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using DIIoCApplication.ViewModels;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace DIIoCApplication
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+        }
+
+
+        public MainWindowViewModel MainViewModel { get { return ServiceLocator.Current.GetInstance<MainWindowViewModel>(); } }
+    }
+}
---
+++ 3nd_sem/se/murrent/DIIoC/DIIoCApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace DIIoCApplication.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
