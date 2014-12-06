51e976df6269761adc0a286f8506c7767c211e06
Mario Murrent <mario@murrent.at>
2014-12-06 11:34:24 +0100

    *) Added cc nodeJs apps

---
+++ 3nd_sem/cc/murrent/e1/01_Exercise_NodeJS/RESTService_NodeJS/server.js
@@ -0,0 +1,60 @@
+﻿var application_root = __dirname,
+    express = require('express'); //Web framework
+
+//Create server
+var app = express();
+
+// Configure server
+app.use(function () {
+    //parses request body and populates request.body
+    app.use(express.bodyParser());
+    
+    //checks request.body for HTTP method overrides
+    app.use(express.methodOverride());
+    
+    //perform route lookup based on url and HTTP method
+    app.use(app.router);
+    
+    //Show all errors in development
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+//Router
+//Get all customers
+app.get('/api/customers', function (request, response) {
+    var customers = [];
+    
+    response.send(customers);
+});
+//Get an order by name
+app.get('/api/order/:name', function (request, response) {
+    var orders = [];
+    
+    response.send(orders);
+});
+
+//Update or add order
+app.put('/api/customer/:name', function (request, response) {
+    response.send("Updated!");
+});
+
+//Update or add order
+app.put('/api/order/:name', function (request, response) {
+    response.send("Updated!");
+});
+
+//Delete an order by anme
+app.delete('/api/order/:name', function (request, response) {
+    response.send("Deleted");
+});
+
+//Delete a customer by name
+app.delete('/api/customer/:name', function (request, response) {
+    response.send("Deleted");
+});
+
+//Start server
+var port = 6666;
+app.listen(port, function () {
+    console.log('Express server listening on port %d in %s mode', port, app.settings.env);
+});
---
+++ 3nd_sem/cc/murrent/e1/01_Exercise_NodeJS/SOAPService_NodeJS/server.js
@@ -0,0 +1,27 @@
+﻿var soap = require('soap-server');
+
+function SOAPWebService() {
+}
+SOAPWebService.prototype.getCustomers = function (all) {
+    return "";
+};
+SOAPWebService.prototype.getOrders = function (customerName) {
+    return "";
+};
+SOAPWebService.prototype.addOrder = function (orderName) {
+    return "";
+};
+SOAPWebService.prototype.addCustomer = function (customerName) {
+    return "";
+};
+SOAPWebService.prototype.deleteOrder = function (orderName) {
+    return "";
+};
+SOAPWebService.prototype.delteCustomer = function (customerName) {
+    return "";
+};
+
+var soapServer = new soap.SoapServer();
+var soapService = soapServer.addService('SOAPWebService', new SOAPWebService());
+
+soapServer.listen(1337, '127.0.0.1');
