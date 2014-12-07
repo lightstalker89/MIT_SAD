c7a21c78338b7b1bc8b0e7da18a849a7fd4579cc
Mario Murrent <mario@murrent.at>
2014-12-07 11:28:51 +0100

    *) Updated qm exercise *) Updated cc project

--- 3nd_sem/cc/murrent/e1/01_Exercise_NodeJS/RESTService_NodeJS/server.js
+++ 3nd_sem/cc/murrent/e1/01_Exercise_NodeJS/RESTService_NodeJS/server.js
@@ -1,5 +1,61 @@
-﻿var application_root = __dirname,
-    express = require('express'); //Web framework
+﻿var application_root = __dirname;
+var express = require('express'); //Web framework
+var _ = require('underscore');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var customers = [];
+var orders = [];
+var createCustomer = function (customerName) {
+    var currentCustomer = _.findWhere(customers, { name: customerName });
+    if (currentCustomer) {
+        return false;
+    } else {
+        customers.push({ name: customerName });
+        return true;
+    }
+};
+
+var createOrder = function (orderName, customerName) {
+    var currentOrder = _.findWhere(orders, { name: orderName });
+    if (currentOrder) {
+        return false;
+    } else {
+        customers.push({ name: orderName, customer: customerName });
+        return true;
+    }
+};
+
+var getOrders = function(customerName) {
+    var currentOrder = _.findWhere(orders, { customerName: customerName });
+    return currentOrder || [];
+};
+
+var deleteOrder = function(orderName) {
+    var currentOrder = _.findWhere(orders, { name: orderName });
+    if (currentOrder) {
+        var index = _.indexOf(order, currentOrder);
+        if (index !== -1) {
+            orders.splice(index, 1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+var deleteCustomer = function (customerName) {
+    var currentCustomer = _.findWhere(customers, { name: customerName });
+    if (currentCustomer) {
+        var index = _.indexOf(customers, currentCustomer);
+        if (index !== -1) {
+            customers.splice(index, 1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
 
 //Create server
 var app = express();
@@ -7,54 +63,74 @@ var app = express();
 // Configure server
 app.use(function () {
     //parses request body and populates request.body
-    app.use(express.bodyParser());
+    app.use(bodyParser());
     
     //checks request.body for HTTP method overrides
-    app.use(express.methodOverride());
-    
-    //perform route lookup based on url and HTTP method
-    app.use(app.router);
+    app.use(methodOverride());
     
     //Show all errors in development
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(logErrors);
+    app.use(clientErrorHandler);
+    app.use(errorHandler);
 });
+
+function logErrors(err, req, res, next) {
+    console.error(err.stack);
+    next(err);
+}
+
+function clientErrorHandler(err, req, res, next) {
+    if (req.xhr) {
+        res.status(500).send({ error: 'Something blew up!' });
+    } else {
+        next(err);
+    }
+}
+
+function errorHandler(err, req, res, next) {
+    res.status(500);
+    res.render('error', { error: err });
+}
 
 //Router
 //Get all customers
-app.get('/api/customers', function (request, response) {
-    var customers = [];
-    
+app.get('/customers', function (request, response) {
     response.send(customers);
 });
-//Get an order by name
-app.get('/api/order/:name', function (request, response) {
-    var orders = [];
-    
-    response.send(orders);
+//Get an order by customer name
+app.get('/order/:name', function (request, response) {
+    response.send(getOrders(request.params.name));
 });
 
 //Update or add order
-app.put('/api/customer/:name', function (request, response) {
-    response.send("Updated!");
+app.put('/customer/:name', function (request, response) {
+    var success = createCustomer(request.params.name);
+    response.send(success);
 });
 
 //Update or add order
-app.put('/api/order/:name', function (request, response) {
-    response.send("Updated!");
+app.put('/order/:name', function (request, response) {
+    var success = createOrder(request.params.name);
+    response.send(success);
 });
 
 //Delete an order by anme
-app.delete('/api/order/:name', function (request, response) {
-    response.send("Deleted");
+app.delete('/order/:name', function (request, response) {
+    var success = deleteOrder(request.params.name);
+    response.send(success);
 });
 
 //Delete a customer by name
-app.delete('/api/customer/:name', function (request, response) {
-    response.send("Deleted");
+app.delete('/customer/:name', function (request, response) {
+    var success = deleteCustomer(request.params.name);
+    return success;
+    response.send(success);
 });
 
 //Start server
 var port = 6666;
-app.listen(port, function () {
-    console.log('Express server listening on port %d in %s mode', port, app.settings.env);
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Express server listening on listening at http://%s:%s', host, port);
 });
