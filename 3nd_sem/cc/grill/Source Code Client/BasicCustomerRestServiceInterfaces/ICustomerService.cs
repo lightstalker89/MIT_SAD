487c6743617159a8bdec9ff546b7199ef2f64b84
Florian Grill <f.grill@live.at>
2014-12-19 17:23:34 +0100

    +) Change cc folder content

---
+++ 3nd_sem/cc/grill/Source Code Client/BasicCustomerRestServiceInterfaces/ICustomerService.cs
@@ -0,0 +1,35 @@
+﻿namespace BasicCustomerRestServiceInterfaces
+{
+    using System.Collections.Generic;
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+    using BasicCustomerRestServiceInterfaces.Models;
+
+    [ServiceContract]
+    public interface ICustomerService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Customers", ResponseFormat = WebMessageFormat.Json)]
+        List<Customer> GetCustomers();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Orders/{customerName}", ResponseFormat = WebMessageFormat.Json)]
+        List<List<string>> GetOrders(string customerName);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Order/{customerName}/order/{order}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        bool AddOrder(string customerName, string order);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "Order/{customerName}/orderIndex/{orderIndex}", ResponseFormat = WebMessageFormat.Json)]
+        bool DeleteOrder(string customerName, string orderIndex);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Customer/{customerName}", ResponseFormat = WebMessageFormat.Json)]
+        bool AddCustomer(string customerName);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "Customer/{customerName}", ResponseFormat = WebMessageFormat.Json)]
+        bool DeleteCustomer(string customerName);
+    }
+}
---
+++ 3nd_sem/cc/grill/Source Code Client/BasicCustomerServiceInterfaces/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace BasicCustomerServiceInterfaces.Models
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public string CustomerName { get; set; }
+
+        [DataMember]
+        public List<List<string>> CustomerOrders { get; set; }
+
+        public Customer()
+        {
+            this.CustomerOrders = new List<List<string>>();
+        }
+    }
+}
