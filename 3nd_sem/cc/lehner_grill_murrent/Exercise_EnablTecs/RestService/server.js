a60ffecc53bee938bf81002f0a4fccbcb0bdccdd
Mario Murrent <mobilemdm@live.at>
2014-12-22 17:51:00 +0100

    *) Added rest service methods for ex 02 hands on

--- 3nd_sem/cc/lehner_grill_murrent/Exercise_EnablTecs/RestService/server.js
+++ 3nd_sem/cc/lehner_grill_murrent/Exercise_EnablTecs/RestService/server.js
@@ -1,1 +1,33 @@
-﻿ 
+﻿var express = require('express');
+var _ = require('underscore');
+var storage = require('node-persist');
+var app = express();
+storage.initSync();
+
+app.get('/machines', function (request, response) {
+  
+});
+
+app.get('/machine/:operatingsystem/:softwarename', function (request, response) {
+
+});
+
+app.put('/machine/:name', function (request, response) {
+
+});
+
+app.put('/appliance/:name', function (request, response) {
+
+});
+
+app.put('/machine/:operation', function (request, response) {
+    
+});
+
+app.put('/machine/:name/:description', function (request, response) {
+
+});
+
+app.put('/machine/:name/:rating/:comment', function (request, response) {
+
+});
