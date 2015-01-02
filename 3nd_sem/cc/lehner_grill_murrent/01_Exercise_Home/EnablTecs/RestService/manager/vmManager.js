f1e1ebc7da8becbeb0dcbd40f79bf16df76789d8
Mario Murrent <mobilemdm@live.at>
2015-01-02 10:55:55 +0100

    *) Renamed project - windoof file path length error

---
+++ 3nd_sem/cc/lehner_grill_murrent/01_Exercise_Home/EnablTecs/RestService/manager/vmManager.js
@@ -0,0 +1,37 @@
+﻿var storageManager = require('manager/storageManager');
+var _ = require('underscore');
+var uuid = require('node-uuid');
+
+var VmManager = function () {
+    var virtualMachines = [];
+    var me = this;
+
+    me.add = function(description) {
+
+    };
+
+    me.getAll = function () {
+        return virtualMachines();
+    };
+
+    me.getMachines = function(operatingSystem, software) {
+
+    };
+
+    me.updateDescription = function(id, description) {
+
+    };
+
+    me.updateRating = function(id, rating, comment) {
+
+    };
+
+    me.start = function(id) {
+
+    };
+
+    me.stop = function(id) {
+
+    };
+};
+module.exports = VmManager;
---
+++ 3nd_sem/cc/lehner_grill_murrent/01_Exercise_Home/EnablTecs/RestService/server.js
@@ -0,0 +1,41 @@
+﻿var express = require('express');
+var vmManager = require('manager/vmManager');
+var fs = require('fs');
+
+var app = express();
+storage.initSync();
+
+/** List all virtual machines **/
+app.get('/machines', function (request, response) {
+    response.write(vmManager.getAll());
+});
+
+/** Search for specific virtual machines by operatin system and software **/
+app.get('/machine/:operatingsystem/:softwarename', function (request, response) {
+
+});
+
+/** Add a new virtual machine **/
+app.put('/machine', function (request, response) {
+    
+});
+
+/** Add a new appliance **/
+app.put('/appliance', function (request, response) {
+
+});
+
+/** Start or stop a virtual machine **/
+app.post('/machine/:id/:operation', function (request, response) {
+    
+});
+
+/** Change the description of a virtual machine **/
+app.post('/machine/:id/:description', function (request, response) {
+
+});
+
+/** Add a rating with a comment to the virtual machine **/
+app.post('/machine/:id/:rating/:comment', function (request, response) {
+
+});
