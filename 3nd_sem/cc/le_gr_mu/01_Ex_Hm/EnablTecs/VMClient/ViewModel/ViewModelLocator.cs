76dc98acdf474c986dbea28ae0a4ec4bc1961ebd
Mario Murrent <mario@murrent.at>
2015-01-06 11:32:45 +0100

    *) Fixed nuget package restore errors

--- 3nd_sem/cc/le_gr_mu/01_Ex_Hm/EnablTecs/VMClient/ViewModel/ViewModelLocator.cs
+++ 3nd_sem/cc/le_gr_mu/01_Ex_Hm/EnablTecs/VMClient/ViewModel/ViewModelLocator.cs
@@ -1,21 +1,16 @@
-/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:VirtualMachineClient"
-                           x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
+﻿/*
+In App.xaml:
+<Application.Resources>
+<vm:ViewModelLocator xmlns:vm="clr-namespace:VirtualMachineClient"
+x:Key="Locator" />
+</Application.Resources>
+In the View:
+DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+You can also use Blend to do all this with the tool's support.
+See http://www.galasoft.ch/mvvm
 */
-
-using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
-
 namespace VirtualMachineClient.ViewModel
 {
     /// <summary>
@@ -30,21 +25,18 @@ namespace VirtualMachineClient.ViewModel
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
             ////if (ViewModelBase.IsInDesignModeStatic)
             ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            //// // Create design time view services and models
+            //// SimpleIoc.Default.Register<IDataService, DesignDataService>();
             ////}
             ////else
             ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            //// // Create run time view services and models
+            //// SimpleIoc.Default.Register<IDataService, DataService>();
             ////}
-
             SimpleIoc.Default.Register<MainViewModel>();
         }
-
         public MainViewModel Main
         {
             get
@@ -52,7 +44,6 @@ namespace VirtualMachineClient.ViewModel
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
             }
         }
-        
         public static void Cleanup()
         {
             // TODO Clear the ViewModels
