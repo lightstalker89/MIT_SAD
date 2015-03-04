1ffb856ee407ec89c6bdb44217dc26c85c3f0ee8
Mario Murrent <mobilemdm@live.at>
2015-03-04 19:48:15 +0100

    *) Project setup for ass

---
+++ 4th_sem/ass/murrent/PassSecure/PassSecure/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace PassSecure
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x101;
+        private LowLevelKeyboardProc proc = HookCallback;
+        private static IntPtr hookID = IntPtr.Zero;
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IntPtr handle = GetConsoleWindow();
+            ShowWindow(handle, 5);  // to hide the running application
+            hookID = SetHook(proc);
+            this.Closing += this.MainWindowClosing;
+            PasswordBox.Focus();
+        }
+
+        protected void MainWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            UnhookWindowsHookEx(hookID); 
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Debug.WriteLine(DateTime.Now.TimeOfDay + ": " + (Keys)vkCode + " Key Down");
+            }
+            else if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Debug.WriteLine(DateTime.Now.TimeOfDay + ": " + (Keys)vkCode + " Key Up");
+            }
+            return CallNextHookEx(hookID, nCode, wParam, lParam);
+        }
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        #region DLL Imports
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        #endregion
+    }
+}
