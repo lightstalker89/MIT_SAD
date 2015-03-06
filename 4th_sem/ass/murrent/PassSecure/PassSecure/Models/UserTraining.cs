ea36fd8cff0a86fb2c7be8c4091fa4ce3fa975c9
Mario Murrent <mobilemdm@live.at>
2015-03-06 16:37:04 +0100

    *) Updated models for ass project

--- 4th_sem/ass/murrent/PassSecure/PassSecure/Models/UserTraining.cs
+++ 4th_sem/ass/murrent/PassSecure/PassSecure/Models/UserTraining.cs
@@ -1,7 +1,12 @@
 ﻿namespace PassSecure.Models
 {
+    using System.Collections.Generic;
+    using System.Windows.Documents;
+
     public class UserTraining
     {
         public string UserName { get; set; }
+
+        public List<TrainingEntry> Trainings { get; set; };
     }
 }
