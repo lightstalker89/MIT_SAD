00736af191e42fde8614fb1a25774242260f2a96
Mario Murrent <mobilemdm@live.at>
2015-03-06 13:02:23 +0100

    *) Updated usercontrol library

---
+++ 4th_sem/ass/murrent/PassSecure/SecurityControls/PersonalQuestionControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+
+namespace SecurityControls
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interaction logic for PersonalQuestionControl.xaml
+    /// </summary>
+    public partial class PersonalQuestionControl
+    {
+        public PersonalQuestionControl()
+        {
+            InitializeComponent();
+        }
+
+        public Dictionary<string, string> PersonalQuestions { get; set; }
+
+        private void CheckAnswer()
+        {
+            
+        }
+    }
+}
