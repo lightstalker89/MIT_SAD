ecc62d16defb40bca4abbd9ec3af96fa54aa5cff
Mario Murrent <mobilemdm@live.at>
2015-03-13 21:01:42 +0100

    *) Updated data

--- 4th_sem/ass/murrent/PassSecure/PassSecure/Models/PasswordEntry.cs
+++ 4th_sem/ass/murrent/PassSecure/PassSecure/Models/PasswordEntry.cs
@@ -6,6 +6,10 @@
 // </summary>
 // <author>Mario Murrent</author>
 #endregion
+
+using System.Collections.Generic;
+using System.Linq;
+
 namespace PassSecure.Models
 {
     #region Usings
@@ -37,5 +41,26 @@ namespace PassSecure.Models
         /// <summary>
         /// </summary>
         public TimeSpan TotalKeyUpTime { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public List<KeyStroke> KeyStrokes { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public void Analyze()
+        {
+            if (KeyStrokes.Count > 1)
+            {
+                TotalTime = KeyStrokes.Last().KeyUpTime - KeyStrokes.First().KeyDownTime;
+            }
+
+            for (int i = 1; i < KeyStrokes.Count; i++)
+            {
+                int lastIndex = i - 1;
+                KeyStrokes[i].TimeToLastKeyDown = KeyStrokes[i].KeyDownTime - KeyStrokes[lastIndex].KeyDownTime;
+                KeyStrokes[i].TimeToLastKeyUp = KeyStrokes[i].KeyUpTime - KeyStrokes[lastIndex].KeyUpTime;
+            }
+        }
     }
 }
