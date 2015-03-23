4f166dbf8c2afb30a38fe62c72a906d4ca8912c4
Roland Lehner <90863@fhwn.ac.at>
2015-03-23 19:09:19 +0100

    - updated MLRParser. File path will be recognized.

--- 4th_sem/ass/lehner/MultipleLineareRegression/src/MLRParser.py
+++ 4th_sem/ass/lehner/MultipleLineareRegression/src/MLRParser.py
@@ -12,8 +12,8 @@ operator = oneOf("+ - * / [ ] . ^ { } %")
 
 alpha = variable("alpha")
 data = oneOf("csv sql excel")
-dataSource = (Word(alphas) + Optional(":\\") + Optional(delimitedList(Word(alphas), delim='\\')))
-dataTerm = data + equal + dataSource
+path = Regex('([A-Z][:][/])?[a-zA-Z_0-9/]*')
+dataTerm = data + equal + path
 dataTerms = delimitedList(dataTerm("dataTerms*"))
 
 termPart  = nestedExpr() | nestedExpr('{','}') | integer | operator
@@ -25,7 +25,7 @@ variables = delimitedList(term("variables*"))
 dsl = keyword + leftBracket + alpha + tild + variables + delimiter + dataTerms + rightBracket + semicolon
 
 #result = dsl.parseString("lm(y ~ x1+2, x2, x3 | csv=mydata,sql=asdf);")
-result = dsl.parseString("lm(Sales ~ TV * 2, 4 / Radio, 3 * (Newspaper ^ 2) | csv=asdf);")
+result = dsl.parseString("lm(Sales ~ TV * 2, 4 / Radio, 3 * (Newspaper ^ 2) | csv=C:/asdf/asdf,sql=F:/hallo/hallo);")
 print result.alpha
 print result.variables
 print result.dataTerms
