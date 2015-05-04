357ff45ff0e90c0ad47c1d784a07dd7b74490a69
Roland Lehner <90863@fhwn.ac.at>
2015-05-04 21:33:46 +0200

    - updated MLRParser for supporting keys in multiple sources.

--- 4th_sem/ass/lehner/MultipleLineareRegression/src/MLRParser.py
+++ 4th_sem/ass/lehner/MultipleLineareRegression/src/MLRParser.py
@@ -29,13 +29,73 @@ variables = delimitedList(term("variables*"))
 dsl = keyword + leftBracket + alpha + tild + variables + delimiter + dataTerms + rightBracket
 inputString = delimitedList(dsl("inputString*"), delim=semicolon)
 
+class DuplicateKeysError(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+	def __str__(self):
+		return repr(self.msg)
+
+class KeyNotFoundError(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+	def __str__(self):
+		return repr(self.msg)
+
+def isKeyDuplicate(variables, sources):
+	for variable in variables:
+		trueCount = 0
+		for source in sources:
+			keys = []
+			if source[0] == 'csv':
+				keys = CSVParser.getKeys(source[1])
+			elif source[0] == 'sql':
+				print "sql"
+			elif source[0] == 'excel':
+				print "excel"
+
+			if variable.beta:
+				if variable.beta in keys:
+					trueCount += 1
+			# has nested term
+			else:
+				for var in variable:
+					if isinstance(var, basestring) is not True:
+						for key in var:
+							if key in keys:
+								trueCount += 1
+		#print trueCount
+		if trueCount > 1:
+			msg = 'Duplicated x-value found in sources: '
+			for sourceString in sources:
+				msg += ' ' + sourceString[1]
+			raise DuplicateKeysError(msg)
+
+def getDataSourceFromKey(key, dataSources):
+	for dataSource in dataSources:
+		keys = []
+		if dataSource[0] == 'csv':
+			keys = CSVParser.getKeys(dataSource[1])
+		elif dataSource[0] == 'sql':
+			keys = [] #TODO SQL Parser
+		elif dataSource[0] == 'excel':	
+			keys = [] #TODO excel Parser
+
+		if isinstance(key, basestring) is not True:
+			for keyValue in key:
+				if keyValue in keys:
+					return dataSource
+		else:
+			if key in keys:
+				return dataSource
+	return ''
+		
+
 #result = dsl.parseString("lm(y ~ x1+2, x2, x3 | csv=mydata,sql=asdf);")
 # result = dsl.parseString(r"lm(Sales ~ TV * 2, 4 / Radio, 3 * (Newspaper ^ 2) | csv=advertising.csv);")
 code = """
-lm(Sales ~ TV * 2, 4 / Radio, 3 * (Newspaper ^ 2) | csv=advertising_test.csv);
+lm(Sales ~ TV * 2, 4 / Radio, 3 * (Newspaper ^ 2) | csv=advertising_sourceA.csv, csv=advertising_sourceB.csv);
 lm(Sales ~ TV, Radio, Newspaper | csv=advertising.csv)
 """
-
 
 result = inputString.parseString(code)
 test = code.split(';')
@@ -45,22 +105,38 @@ test = code.split(';')
 # print result.beta
 idx = 0
 for linearRegression in result.inputString:
+	isKeyDuplicate(linearRegression.variables, linearRegression.dataTerms)
+	print ''
 	print test[idx].strip()
-	for dataSource in linearRegression.dataTerms:
-		if dataSource[0] == 'csv':
-			dataArray = []
-			for term in linearRegression.variables:
-				dataArray.append(CSVParser.csvDataFromTerm(term, dataSource[1]))
-			xArray = []
-			for i in range(len(dataArray)):
-				xArray.append(dataArray[i])
-			MLRCalc.calcCoeff(CSVParser.csvDataFromTerm([linearRegression.alpha], dataSource[1]), xArray)
-			# MLRCalc.calcCoeff([3], [[3,7,4,2], [6,7,3,9]])
-			# result = MLRCalc.invert([[2,1,1], [3,2,1], [2,1,2]])
-			# print "Inverted Matrix:"
-			# print result
-		elif dataSource[0] == 'sql':
-			print 'asdf'
-		elif dataSource[0] == 'excel':	
-			print 'asdf'
+	dataArray = []
+	for term in linearRegression.variables:
+		for key in term:
+			data = getDataSourceFromKey(key, linearRegression.dataTerms)
+			if data:
+				if data[0] == 'csv':
+					dataArray.append(CSVParser.csvDataFromTerm(term, data[1]))
+				elif data[0] == 'sql':
+					print 'sql'
+				elif data[0] == 'excel':
+					print 'excel'
+
+	xArray = []
+	for i in range(len(dataArray)):
+		xArray.append(dataArray[i])
+
+	alphaDataSource = getDataSourceFromKey(linearRegression.alpha, linearRegression.dataTerms)
+	if alphaDataSource:
+		data = []
+		if alphaDataSource[0] == 'csv':
+			data = CSVParser.csvDataFromTerm([linearRegression.alpha], alphaDataSource[1])
+		elif alphaDataSource[0] == 'sql':
+			print 'sql'
+		elif alphaDataSource[0] == 'excel':
+			print 'excel'
+		MLRCalc.calcCoeff(data, xArray)
+	else:
+		raise KeyNotFoundError('Key not found in sources: ' + linearRegression.alpha)
 	idx += 1
+
+
+
