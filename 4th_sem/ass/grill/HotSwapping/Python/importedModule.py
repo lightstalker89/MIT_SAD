fff33f22e921c8fc59c1307728981002874662d2
Florian Grill <f.grill@live.at>
2015-05-04 17:01:29 +0200

    +) Add Python initial project

---
+++ 4th_sem/ass/grill/HotSwapping/Python/importedModule.py
@@ -0,0 +1,8 @@
+class ModuleClass(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    # this method can be changed at runtime
+    def doSomething():
+        print("Reloadssjjjgshh")
